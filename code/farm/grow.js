b4aceb40ea2f90f9ebae46a554a833a3c0ddeb0c
Daniel Ebrahimian <git@danielebra.com>
2024-01-05 14:17:18 +1100

    Add port communication to commands

--- code/farm/grow.js
+++ code/farm/grow.js
@@ -1,5 +1,15 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    const target = ns.args[0];
-    await ns.grow(target);
+  const [target, port, jobID] = ns.args;
+
+  const COMPLETED = `${jobID}#COMPLETE`;
+  const STARTED = `${jobID}#STARTED`;
+
+  ns.tryWritePort(port, STARTED);
+  ns.print(STARTED)
+
+  await ns.grow(target);
+
+  ns.tryWritePort(port, COMPLETED);
+  ns.print(COMPLETED)
 }
--- code/farm/weaken.js
+++ code/farm/weaken.js
@@ -1,5 +1,15 @@
-/** @param {NS} ns */
+/** @param {NS} ns **/
 export async function main(ns) {
-  const target = ns.args[0];
+  const [target, port, jobID] = ns.args;
+
+  const COMPLETED = `${jobID}#COMPLETE`;
+  const STARTED = `${jobID}#STARTED`;
+
+  ns.tryWritePort(port, STARTED);
+  ns.print(STARTED)
+
   await ns.weaken(target);
+
+  ns.tryWritePort(port, COMPLETED);
+  ns.print(COMPLETED)
 }
