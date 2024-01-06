af8b8866c3008b701d84f21bac46a04586829bf3
Daniel Ebrahimian <git@danielebra.com>
2024-01-06 18:55:54 +1100

    Begin implementing batcher

--- code/farm/playground.js
+++ code/farm/playground.js
@@ -1,11 +1,33 @@
 import { Cluster } from "/code/farm/cluster.js";
+import { analyzeServer } from "/code/farm/inspector.js";
+import { SCRIPTS } from "/code/utils.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
-  const cluster = new Cluster(ns)
-  ns.tprint(cluster.getTotalRam())
-  ns.tprint(cluster.getTotalUsedRam())
-  ns.tprint(cluster.getTotalAvailableRam())
+  const target = "n00dles";
+  const manager = new HWGW(ns, target);
+  while (!manager.prepareTarget()) {
+    ns.print("INFO", `Preparing ${target}`);
+  }
+  ns.print("Ready");
+}
 
-  ns.tprint(cluster.getUtilizationPercentage())
+class HWGW {
+  constructor(ns, target) {
+    this.ns = ns;
+    this.cluster = new Cluster(ns);
+    this.target = target;
+  }
+  async prepareTarget() {
+    const state = analyzeServer(this.ns, this.target);
+    if (state.additionalSecurity > 0) {
+      await this.cluster.distribute(SCRIPTS.WEAKEN, state.weakenThreadsNeeded, this.target);
+      return false;
+    } else if (state.moneyBalancePercentage < 100) {
+      await this.cluster.distribute(SCRIPTS.WEAKEN, state.weakenThreadsNeeded, this.target);
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
