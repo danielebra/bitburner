a1b90d637b6a74552432be7630fec254804eacfa
Daniel Ebrahimian <git@danielebra.com>
2024-01-11 23:04:24 +1100

    update find to output connect command

--- code/find.js
+++ code/find.js
@@ -1,37 +1,37 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    let target = ns.args[0]
+  let target = ns.args[0];
 
-    let path = traverse("home", target, "home")
-    if (path == "") {
-        ns.tprint("Could not find " + target)
-        return
+  let path = traverse("home", target, "home");
+  if (path == "") {
+    ns.tprint("Could not find " + target);
+    return;
+  }
+
+  ns.tprint(path);
+
+  function traverse(server, target, from) {
+    if (server == target) {
+      return target;
     }
-
-    ns.tprint(path)
-
-    function traverse(server, target, from) {
-        if (server == target) {
-            return target
-        }
-        let nodes = ns.scan(server)
-        for (let i = 0; i < nodes.length; i++) {
-            let child = nodes[i]
-            if (child == from) {
-                continue
-            }
-            if (child == target) {
-                return server + " => " + target
-            }
-            let children = ns.scan(server)
-            if (children.length === 0) {
-                continue
-            }
-            let foundOn = traverse(child, target, server)
-            if (foundOn != "") {
-                return server + " => " + foundOn
-            }
-        }
-        return ""
+    let nodes = ns.scan(server);
+    for (let i = 0; i < nodes.length; i++) {
+      let child = nodes[i];
+      if (child == from) {
+        continue;
+      }
+      if (child == target) {
+        return server + "; connect " + target;
+      }
+      let children = ns.scan(server);
+      if (children.length === 0) {
+        continue;
+      }
+      let foundOn = traverse(child, target, server);
+      if (foundOn != "") {
+        return server + "; connect " + foundOn;
+      }
     }
+    return "";
+  }
 }
